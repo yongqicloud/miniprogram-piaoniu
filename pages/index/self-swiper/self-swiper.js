3d776b8ef02f575258e94ba8b4e9b50f27691d61
liyongqi <xinda_li@163.com>
2020-01-03 23:11:32 +0800

    2020-1-3搜索组件壳封装&&swiper

---
+++ pages/index/self-swiper/self-swiper.js
@@ -0,0 +1,27 @@
+// pages/index/self-swiper/self-swiper.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    background:[
+      'https://img.piaoniu.com/misc/ed0410f016c8093d336561b859c9e35588a28c37.png?imageView2/2/w/750/h/350/format/jpg/q/80',
+      'https://img.piaoniu.com/misc/7c5215312ff0ee76ba99214b11a2c303a7a8ae88.png?imageView2/2/w/750/h/350/format/jpg/q/80',
+    ]
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
