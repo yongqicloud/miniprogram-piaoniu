4eca71312544ddd70fdb7e64872ff192e7569186
liyongqi <xinda_li@163.com>
2020-01-03 19:33:22 +0800

    2020-1-3项目初始化

---
+++ app.js
@@ -0,0 +1,17 @@
+// "tabBar": {
+  //   "custom": true,
+  //   "color": "#7A7E83",
+  //   "selectedColor": "#3cc51f",
+  //   "borderStyle": "black",
+  //   "backgroundColor": "#ffffff",
+  //   "list": [
+  //     {
+  //       "pagePath": "pages/index/index",
+  //       "text": "组件"
+  //     },
+  //     {
+  //       "pagePath": "pages/details/details",
+  //       "text": "接口"
+  //     }
+  //   ]
+  // },
