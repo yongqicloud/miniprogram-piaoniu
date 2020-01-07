2ae236922d42454c71c1eb004bff5d7f2f20c046
luotingo <luotingo@126.com>
2020-01-07 08:18:17 +0800

    2020-1-7 tabbar和粗略的条件选择

--- pages/perform/perform.js
+++ pages/perform/perform.js
@@ -11,13 +11,26 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+		show:null,
+		color:false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+	
+		showComment:function(){
+			// console.log(this)
+			this.setData({
+				show: "comment"
+			})
+		},
+		showTime:function(event){
+			this.setData({
+				show:"time",
+				color:true
+			})
+		}
   }
 })
