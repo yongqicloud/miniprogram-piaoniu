2649d0620c29e69bc8256494f64f5ac2b061b235
liyongqi <xinda_li@163.com>
2020-01-07 23:17:38 +0800

    2020-1-7李永琪nav-swiper渲染

--- pages/index/nav-swiper/nav-swiper.js
+++ pages/index/nav-swiper/nav-swiper.js
@@ -1,6 +1,5 @@
 // pages/index/nav-swiper/nav-swiper.js
 const app = getApp()
-console.log(app)
 Component({
   /**
    * 组件的属性列表
@@ -13,19 +12,46 @@ Component({
    * 组件的初始数据
    */
   data: {
-    
+    activeClass:'精彩推荐',
+    list:[]
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    checkoutNav(e){
+      console.log(e)
+      let { title, categoryid} = e.currentTarget.dataset
+      
+      wx.request({
+        url: 'https://m.piaoniu.com/api/v3/activities?pageIndex=1&categoryId=' + categoryid,
+        method:'GET',
+        success:(res)=> {
+          // console.log(res.data)
+          this.setData({
+            activeClass: title,
+            list:res.data.data
+          })
+        }
+      })
+    },
+    fetchData:()=>{
 
+    }
   },
   lifetimes:{
     attached(){
-      this.setData({
-        list:app.nav_list
+      wx.request({
+        url: 'https://m.piaoniu.com/api/v3/activities?pageIndex=1&categoryId=0',
+        method: 'GET',
+        success: (res) => {
+          console.log(res.data)
+          this.setData({
+            list: res.data.data,
+            name_list: app.nav_list
+          })
+        }
       })
     }
   }
